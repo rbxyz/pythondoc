308b18c549405384f4a9ad364af6f5e592d5229e
Ruan Gabriel <110505289+rbxyz@users.noreply.github.com>
2024-09-21 19:38:22 -0300

    server_webv2

--- main.py
+++ main.py
@@ -1,58 +1,53 @@
+from flask import Flask, request, jsonify, render_template
 from openai import OpenAI
-from langchain_openai import OpenAI
 from dotenv import load_dotenv
-import getpass
 import os
 
-#chamando o .env
+app = Flask(__name__)
 load_dotenv()
 
 id = "asst_a1M3LQjGhY4hgVKFJJhNoYGk"
 api_key = os.getenv("OPENAI_API_KEY")
-
 client = OpenAI(api_key=api_key)
 
-#cria o arquivo e 'envia' para o agente
-file = client.files.create(
-    file=open("jogadores.pdf", "rb"),
-    purpose = "assistants"
-)
-print(file)
-print('==============')
+#app que define umaa rota
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-#cria a thread e começa o chat
-thread = client.beta.threads.create()
-print("Bola na rede. 'exit' pra sair.")
+#rota que aceita request post pra interagir com o BallAI
+@app.route('/conversa', methods=['POST'])
+def conversa():
+    user_input = request.json.get('message')
+    if user_input.lower() == 'exit':
+        return jsonify({'resposta': 'bola fora.'})
+    #cria thread
 
-while True:
-  #entrada
-  user_input = input("user: ")
-  if user_input.lower() == 'exit':
-        print("conversa finalizada.")
-        break
-
-  #envia mensagem
-  message = client.beta.threads.messages.create(
+    thread = client.beta.threads.create()
+    #cria o escopo da mensagem
+    message = client.beta.threads.messages.create(
         thread_id=thread.id,
         role="user",
         content=user_input
     )
-
-  #roda o assistente
-  run = client.beta.threads.runs.create_and_poll(
+    #roda o assitente
+    run = client.beta.threads.runs.create_and_poll(
         thread_id=thread.id,
         assistant_id=id
     )
-
-  # resposta do assistente
-  response = client.beta.threads.runs.retrieve(
+    #assistente responde
+    resposta = client.beta.threads.runs.retrieve(
         thread_id=thread.id,
         run_id=run.id
     )
-
-  #envia a resposta
-  messages = client.beta.threads.messages.list(thread_id=thread.id)
-  for msg in reversed(messages.data):
+    #lista a mensagem
+    messages = client.beta.threads.messages.list(thread_id=thread.id)
+    for msg in reversed(messages.data):
         if msg.role == "assistant":
-            print("assistente:", msg.content[0].text.value)
-print('==============')
+            assistant_resposta = msg.content[0].text.value
+            return jsonify({'resposta': assistant_resposta})
+    #fallback
+    return jsonify({'resposta': 'Nenhuma resposta recebida.'})
+#executa o app
+if __name__ == '__main__':
+    app.run(debug=True)
