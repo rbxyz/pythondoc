7e4b73a5ec9eef918ba90e7ecc71ec2a451a0f1a
Ruan Gabriel <110505289+rbxyz@users.noreply.github.com>
2024-09-21 12:07:45 -0300

    projeto funcional

--- main.py
+++ main.py
@@ -1,44 +1,67 @@
 from openai import OpenAI
-import requests
-# thread id:
-# thread_w0z7dPj37IKlphvIkdVDNQL2
+from dotenv import load_dotenv
+import os
 
-# run id:
-# run_M1H6hyxoGOnpM28wxI57Cn1o
+#chamando o .env
+load_dotenv()
 
-# assistant id:
-# asst_a2VHUgz8cN2Jc7p38GNDgSwO
+id = "asst_a1M3LQjGhY4hgVKFJJhNoYGk"
+api_key = os.getenv("OPENAI_API_KEY")
 
-client = OpenAI(
-    api_key='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'
+client = OpenAI(api_key=api_key)
+
+#cria o arquivo e 'envia' para o agente
+file = client.files.create(
+    file=open("regras_futebol.pdf", "rb"),
+    purpose = "assistants"
 )
- #criando o agente
+print(file)
+print('==============')
+
+#cria o agente
 assistant = client.beta.assistants.create(
-  name="Analista Técnico de Futebol",
-  instructions="analise os times de futebol e indique quais tem maiores pretenções de vencer o brasileirao de acordo com a quantidade de gols registrados ate agora, sendo que se necessário te enviarei a quantidade.",
-  model="gpt-3.5-turbo",
-  tools=[{"type": "code_interpreter"}],
+    name="Analista Técnico de Futebol",
+    instructions="Analise as questões sobre placares de futebol.",
+    model="gpt-3.5-turbo",
+    tool_resources={
+    "code_interpreter": {
+      "file_ids": [file.id]
+    }
+  }
 )
+print("Assistente criado:", assistant)
+print('==============')
 
-#criando a Thread
-thread = client.beta.threads.create()
-
-#add msg na Thread
-message = client.beta.threads.messages.create(
+#cria a thread e a requisicao
+thread = client.beta.threads.create(
+  messages=[
+    {
+      "role": "user",
+      "content": "Faça um breve resumo sobre o arquivo anexo",
+      "attachments": [
+        {
+          "file_id": file.id,
+          "tools": [{"type": "code_interpreter"}]
+        }
+      ]
+    }
+  ]
+)
+#roda o assistente
+run = client.beta.threads.runs.create_and_poll(
   thread_id=thread.id,
-  role="user",
-  content="Análise entre o Grêmio e o Internacional até agora e indique qual tem maior probabilidade de ganhar de acordo com a pontuação de gols atual."
+  assistant_id=id
 )
 
-run = client.beta.threads.runs.create(
+#resposta assistente
+run = client.beta.threads.runs.retrieve(
   thread_id=thread.id,
-  assistant_id=assistant.id
+  run_id=run.id
 )
-if run.status == 'completed': 
-    messages = client.beta.threads.messages.list(
+messages = client.beta.threads.messages.list(
     thread_id=thread.id
-    )
-    print(messages)
-else:
-    print(run.status)
+)
+for message in reversed(messages.data):
+    print(message.role + ": " + message.content[0].text.value)
+
 
