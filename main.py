e071db209669a49ded5633fc1e07a083ca3fde22
Ruan Gabriel <110505289+rbxyz@users.noreply.github.com>
2024-09-21 14:48:10 -0300

    v3

--- main.py
+++ main.py
@@ -12,56 +12,60 @@ client = OpenAI(api_key=api_key)
 
 #cria o arquivo e 'envia' para o agente
 file = client.files.create(
-    file=open("regras_futebol.pdf", "rb"),
+    file=open("jogadores.pdf", "rb"),
     purpose = "assistants"
 )
 print(file)
 print('==============')
 
 #cria o agente
-assistant = client.beta.assistants.create(
-    name="Analista Técnico de Futebol",
-    instructions="Analise as questões sobre placares de futebol.",
-    model="gpt-3.5-turbo",
-    tool_resources={
-    "code_interpreter": {
-      "file_ids": [file.id]
-    }
-  }
-)
-print("Assistente criado:", assistant)
+#assistant = client.beta.assistants.create(
+#    name="Analista Técnico de Futebol",
+#    instructions="Analise as questões sobre placares de futebol.",
+#    model="gpt-3.5-turbo",
+#    tool_resources={
+#    "code_interpreter": {
+#      "file_ids": [file.id]
+#    }
+#  }
+#)
+
+#print("Assistente criado:", assistant)
+#print('==============')
+
+#cria a thread e começa o chat
+thread = client.beta.threads.create()
+print("Bola na rede. 'exit' pra sair.")
+
+while True:
+  #entrada
+  user_input = input("user: ")
+  if user_input.lower() == 'exit':
+        print("conversa finalizada.")
+        break
+
+  #envia mensagem
+  message = client.beta.threads.messages.create(
+        thread_id=thread.id,
+        role="user",
+        content=user_input
+    )
+
+  #roda o assistente
+  run = client.beta.threads.runs.create_and_poll(
+        thread_id=thread.id,
+        assistant_id=id
+    )
+
+  # resposta do assistente
+  response = client.beta.threads.runs.retrieve(
+        thread_id=thread.id,
+        run_id=run.id
+    )
+
+  #envia a resposta
+  messages = client.beta.threads.messages.list(thread_id=thread.id)
+  for msg in reversed(messages.data):
+        if msg.role == "assistant":
+            print("assistente:", msg.content[0].text.value)
 print('==============')
-
-#cria a thread e a requisicao
-thread = client.beta.threads.create(
-  messages=[
-    {
-      "role": "user",
-      "content": "Faça um breve resumo sobre o arquivo anexo",
-      "attachments": [
-        {
-          "file_id": file.id,
-          "tools": [{"type": "code_interpreter"}]
-        }
-      ]
-    }
-  ]
-)
-#roda o assistente
-run = client.beta.threads.runs.create_and_poll(
-  thread_id=thread.id,
-  assistant_id=id
-)
-
-#resposta assistente
-run = client.beta.threads.runs.retrieve(
-  thread_id=thread.id,
-  run_id=run.id
-)
-messages = client.beta.threads.messages.list(
-    thread_id=thread.id
-)
-for message in reversed(messages.data):
-    print(message.role + ": " + message.content[0].text.value)
-
-
